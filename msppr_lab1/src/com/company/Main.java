0f3db52c846442ea768f5e903dd0f9cc43a18713
markgardie <markgardie@users.noreply.github.com>
2022-01-30 19:16:37 +0200

    first version

--- msppr_lab1/src/com/company/Main.java
+++ msppr_lab1/src/com/company/Main.java
@@ -12,8 +12,9 @@ public class Main {
     public static void main(String[] args) {
 
 
-        ArrayList<Integer> q1 = new ArrayList<>(Arrays.asList(2, 6, 2, 6, 8, 1, 6, 2, 8, 9, 1, 3, 5, 6, 0, 6, 6, 9, 9, 6));
-        ArrayList<Integer> q2 = new ArrayList<>(Arrays.asList(0, 3, 1, 0, 3, 6, 7, 3, 2, 2, 1, 5, 3, 3, 8, 2, 8, 5, 8, 0));
+        int[] q1 = new int[] {2, 6, 2, 6, 8, 1, 6, 2, 8, 9, 1, 3, 5, 6, 0, 6, 6, 9, 9, 6};
+        int [] q2 = new int[] {0, 3, 1, 0, 3, 6, 7, 3, 2, 2, 1, 5, 3, 3, 8, 2, 8, 5, 8, 0};
+
         slater(q1, q2);
         pareto(q1, q2);
 
@@ -21,22 +22,25 @@ public class Main {
 
 
 
-    private static void pareto(ArrayList<Integer> q1, ArrayList<Integer>  q2) {
+    private static void pareto(int[] q1, int[]  q2) {
         int i = 0;
         int j = 1;
 
-        while (i < q1.size() - 1 && j < q1.size()) {
-            if (q1.get(i) >= q1.get(j) && q2.get(i) >= q2.get(j)) {
-                q1.remove(j);
-                q2.remove(j);
-                i = 0;
-                j = 1;
+        while (i < q1.length - 2) {
+            if (j == q1.length - 1) {
+                i++;
+                j = i+1;
             }
-            else if (q1.get(i)  <= q1.get(j) && q2.get(i) <= q2.get(j)) {
-                q1.remove(i);
-                q2.remove(i);
-                i = 0;
-                j = 1;
+            else if (q1[i] >= q1[j] && q2[i]>= q2[j]) {
+                q1[j] = 0;
+                q2[j] = 0;
+                j++;
+            }
+            else if (q1[i] <= q1[j] && q2[i] <= q2[j]) {
+                q1[i] = 0;
+                q2[i] = 0;
+                i++;
+                j = i+1;
             }
             else {
                 j++;
@@ -45,28 +49,33 @@ public class Main {
 
 
         System.out.println("Множина значень за Парето:");
-        for (i = 0; i < q1.size(); i++) {
-            System.out.println(q1.get(i) + ":" + q2.get(i));
+        for (i = 0; i < q1.length; i++) {
+            if (q1[i] != 0 && q2[i] != 0) {
+                System.out.println(q1[i] + ";" + q2[i]);
+            }
         }
 
     }
 
-    private static void slater(ArrayList<Integer> q1, ArrayList<Integer>  q2) {
+    private static void slater(int[] q1, int[]  q2) {
         int i = 0;
         int j = 1;
 
-        while (i < q1.size() - 1 && j < q1.size()) {
-            if (q1.get(i) > q1.get(j) && q2.get(i) > q2.get(j)) {
-                q1.remove(j);
-                q2.remove(j);
-                i = 0;
-                j = 1;
+        while (i < q1.length - 2) {
+            if (j == q1.length - 1) {
+                i++;
+                j = i+1;
             }
-            else if (q1.get(i)  < q1.get(j) && q2.get(i) < q2.get(j)) {
-                q1.remove(i);
-                q2.remove(i);
-                i = 0;
-                j = 1;
+            else if (q1[i] > q1[j] && q2[i]> q2[j]) {
+                q1[j] = 0;
+                q2[j] = 0;
+                j++;
+            }
+            else if (q1[i] < q1[j] && q2[i] < q2[j]) {
+                q1[i] = 0;
+                q2[i] = 0;
+                i++;
+                j = i+1;
             }
             else {
                 j++;
@@ -75,8 +84,10 @@ public class Main {
 
 
         System.out.println("Множина значень за Слейтером:");
-        for (i = 0; i < q1.size(); i++) {
-            System.out.println(q1.get(i) + ":" + q2.get(i));
+        for (i = 0; i < q1.length; i++) {
+            if (q1[i] != 0 && q2[i] != 0) {
+                System.out.println(q1[i] + ";" + q2[i]);
+            }
         }
 
     }
